0cd4317ba20764512b524cf056852b8aa6715db0
aspcodenet <stefan.holmberg@systementor.se>
2023-10-24 12:28:16 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+stringcompare
+strncmp
+*/
+int main() {
+    int n = 0;
+    if(n > 120){
+        printf("DASASDASDDA");
+    }
+    bool isCool = false;
+    if(isCool){
+
+    }
+    // int i ;
+    // short int;
+    // long int;
+    // long long int;
+    // float f;
+    // double d;
+    // char ch;
+
+    //String name;
+    //int stefansSalary = 25000;
+    char name[10];
+    // assdsdadasasddasasds
+    scanf(" %s", name);
+    int year;
+    scanf(" %d", &year);
+
+
+    int birthYear = 13;
+    birthYear = birthYear + 1;
+    birthYear += 1;
+    birthYear++;
+
+    birthYear = 12;
+    // mata in ett namn
+
+    // skriva det
+
+    int year;
+    year = 1972; // this is the year
+    float percent = 0.2f;
+    printf("Hello you were born %d it seems and you have %.2f percent discount \n", 
+        year, percent);
+    return 0;
+}
